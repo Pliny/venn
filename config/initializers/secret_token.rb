4e73e81eb1c1433eb320284bd7ac9537fdea2069
Dave Desrochers <dave726@gmail.com>
2014-02-16 15:33:10 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Venn::Application.config.secret_key_base = 'aca5b89068653aa580f59a7814699ff4b8d13625a919b711f685f99f4acd82ff52aaa2f1c2b02192a27740609d0ead5d18547440ccb9ee53ff47904bc42a5286'
